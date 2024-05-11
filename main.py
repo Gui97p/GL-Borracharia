f5c4f225bdadd2d3583884293d49751bb0a60be3
Gui97p <guipassaroto07@gmail.com>
2024-05-10 23:05:12 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+import numpy as np
+from flask import Flask, request
+import pandas as pd
+
+def handleNonNumericalData(df):
+    columns = df.columns.values
+    for column in columns:
+        text_digit_vals = {}
+        def convert_to_int(val):
+            return text_digit_vals[val]
+
+        if df[column].dtype != np.int64 and df[column].dtype != np.float64:
+            column_contents = df[column].values.tolist()
+            unique_elements = set(column_contents)
+            x = 1
+            for unique in unique_elements:
+                if unique not in text_digit_vals:
+                    text_digit_vals[unique] = x
+                    x+=1
+
+            df[column] = list(map(convert_to_int, df[column]))
+
+    return df
+
+df = pd.read_csv('./dataset.csv')
+df = df[~df.isin(['?']).any(axis=1)]
+
+specs = handleNonNumericalData(df[[
+    "make",
+    "fuel-type", 
+    "num-of-doors",
+    "body-style",
+    "drive-wheels",
+    "engine-location",
+    "num-of-cylinders",
+    "horsepower",
+    "peak-rpm"
+]])
+price = df['price']
+
+AI = DecisionTreeClassifier()
+AI = AI.fit(specs, price)
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def hello_world():
+    global AI
+    pred = 'Error on getting the price.'
+
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        data = np.array(list(json.values()))
+        pred = AI.predict([data])
+    
+    return {"price": pred[0]}
