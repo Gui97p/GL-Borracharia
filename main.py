76954f7f6d20d7963765c7ce1474b34b8e7628d6
Gui97p <guipassaroto07@gmail.com>
2024-05-10 23:33:04 -0300

    Changes on host

--- main.py
+++ main.py
@@ -58,3 +58,7 @@ def hello_world():
         pred = AI.predict([data])
     
     return {"price": pred[0]}
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
